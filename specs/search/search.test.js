c46c727137498359022a37ede1d236f46cc50277
MarianaES <mariana.enriquez.s@gmail.com>
2022-01-27 12:36:15 -0600

    Add linear and binary search solution

--- specs/search/search.test.js
+++ specs/search/search.test.js
@@ -4,16 +4,53 @@
 // it's up to you what to return if the object isn't found (we're not testing that)
 
 function linearSearch(id, array) {
-  // code goes here
+  for (let i = 0; i < array.length; i++) {
+    if (id === array[i].id) {
+      return array[i];
+    }
+  }
+  return 0;
 }
 
+// MUST BE a sorted array
 function binarySearch(id, array) {
-  // code goes here
-}
+  let start = 0;
+  let end = array.length - 1;
 
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    if (array[middle].id === id) {
+      return array[middle];
+    } else if (array[middle].id > id) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+
+  return 0;
+}
+console.log(
+  binarySearch(23, [
+    { id: 1, name: "Sam" },
+    { id: 3, name: "Sarah" },
+    { id: 5, name: "John" },
+    { id: 6, name: "Burke" },
+    { id: 10, name: "Simona" },
+    { id: 12, name: "Asim" },
+    { id: 13, name: "Niki" },
+    { id: 15, name: "Aysegul" },
+    { id: 17, name: "Kyle" },
+    { id: 18, name: "Jem" },
+    { id: 19, name: "Marc" },
+    { id: 21, name: "Chris" },
+    { id: 23, name: "Brian" },
+    { id: 24, name: "Ben" },
+  ])
+);
 // unit tests
 // do not modify the below code
-test.skip("linear search", function () {
+test("linear search", function () {
   const lookingFor = { id: 5, name: "Brian" };
   expect(
     linearSearch(5, [
@@ -30,12 +67,12 @@ test.skip("linear search", function () {
       { id: 2, name: "Marc" },
       { id: 51, name: "Chris" },
       lookingFor,
-      { id: 14, name: "Ben" }
+      { id: 14, name: "Ben" },
     ])
   ).toBe(lookingFor);
 });
 
-test.skip("binary search", function () {
+test("binary search", function () {
   const lookingFor = { id: 23, name: "Brian" };
   expect(
     binarySearch(23, [
@@ -52,7 +89,7 @@ test.skip("binary search", function () {
       { id: 19, name: "Marc" },
       { id: 21, name: "Chris" },
       lookingFor,
-      { id: 24, name: "Ben" }
+      { id: 24, name: "Ben" },
     ])
   ).toBe(lookingFor);
 });
